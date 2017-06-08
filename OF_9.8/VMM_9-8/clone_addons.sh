6f11ec7358ca6ed865363d2ee424140e988bce9f
Robert White III <lg3@LG3-MBP.fios-router.home>
2017-06-07 23:20:38 -0400

    fixing clone_addons.sh

--- OF_9.8/VMM_9-8/clone_addons.sh
+++ OF_9.8/VMM_9-8/clone_addons.sh
@@ -12,7 +12,7 @@ cd ../../../addons
 
 #ofxJSON--------------------------------
 #https://github.com/jeffcrouse/ofxJSON/tree/5934d7044406041d46c763d0509613ac71801256
-${PREFIX}comoc/jeffcrouse/ofxJSON.git
+${PREFIX}jeffcrouse/ofxJSON.git
 
 #ofxObjLoader---------------------------
 #https://github.com/andreasmuller/ofxObjLoader/commit/fc5bbb1bc7efd396cd46cd6e312669cb4a362ba0
