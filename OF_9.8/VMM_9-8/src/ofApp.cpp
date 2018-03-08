c2a88477daf7f8240a9ed05e9328723b0851ec1c
lg3bass <lg3bass@gmail.com>
2018-03-08 18:14:31 -0500

    logLevels

--- OF_9.8/VMM_9-8/src/ofApp.cpp
+++ OF_9.8/VMM_9-8/src/ofApp.cpp
@@ -8,11 +8,11 @@ void ofApp::setup(){
     //LOGGING
     //http://openframeworks.cc/documentation/utils/ofLog.html
     ofSetLogLevel("jsonData", OF_LOG_ERROR);//DEFAULT: OF_LOG_ERROR
-    ofSetLogLevel("OSC",OF_LOG_VERBOSE);//DEFAULT: OF_LOG_VERBOSE
+    ofSetLogLevel("OSC",OF_LOG_ERROR);//DEFAULT: OF_LOG_VERBOSE
     ofSetLogLevel("matcap",OF_LOG_SILENT);//DEFAULT: OF_LOG_ERROR
     ofSetLogLevel("ofxUI",OF_LOG_SILENT);//DEFAULT: OF_LOG_SILENT
     ofSetLogLevel("objloader", OF_LOG_SILENT);//DEFAULT: OF_LOG_NOTICE
-    ofSetLogLevel("LINK", OF_LOG_VERBOSE);
+    ofSetLogLevel("LINK", OF_LOG_SILENT);
     
     
     //SYPHON
